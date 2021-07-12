d7258d91a937f898e74db489a4afc4e01b646a58
SuzukiKatsuma <71871253+SuzukiKatsuma@users.noreply.github.com>
2021-07-13 02:23:19 +0900

    Fix: FizzBuzz.php
    :HTMLでの入力から標準入力に修正
    :厳密モードでの型検査が行われるよう修正
    : == をより厳密に === へ修正

--- PHP/FizzBuzz.php
+++ PHP/FizzBuzz.php
@@ -1,32 +1,13 @@
-<html>
+<?php
+  declare(strict_types=1);
 
-<head>
-  <title>FizzBuzz</title>
-</head>
-
-<body>
-  <form method="post" align="center">
-    <input type="number" name="number">
-    <input type="submit">
-  </form>
-
-  <p align="center">
-    <?php
-      if($_SERVER['REQUEST_METHOD']=="POST") {
-        $x = $_POST['number'];
-        print $x." ";
-
-        if($x % 15 == 0)
-          echo "FizzBuzz";
-        else if($x % 5 == 0)
-          echo "Buzz";
-        else if($x % 3 == 0)
-          echo "Fizz";
-        else
-          echo $x;
-      }
-    ?>
-  </p>
-</body>
-
-</html>
+  $x = trim(fgets(STDIN));
+  if($x % 15 === 0)
+    echo "FizzBuzz";
+  else if($x % 5 === 0)
+    echo "Buzz";
+  else if($x % 3 === 0)
+    echo "Fizz";
+  else
+    echo $x;
+?>
